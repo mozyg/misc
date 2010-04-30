b84a1dc3c12bd91f8da4e382ceacfd467dd31338
Will Dietz <w@wdtz.org>
2010-04-29 21:56:46 -0500

    mount_xterm: add mkdir's, few more mounts

--- vmctl/mount_xterm.sh
+++ vmctl/mount_xterm.sh
@@ -1,18 +1,22 @@
 # This script is supposed to be run inside a new namespace using the newns binary.
 # Example invocation: /media/internal/newns mount_xterm.sh
 #
-# Assumes that there exists a /usr/local/lib directory and an empty file
-# in /usr/local/bin/xterm
-# TODO: Should we try to create the directory and file in this script ?
-#
+
 
 JAIL_DIR=/media/internal/jail
 XLIB_DIR=/media/cryptofs/apps/usr/palm/applications/org.webosinternals.xlib
 XTERM_BIN=/media/cryptofs/apps/usr/palm/applications/org.webosinternals.xterm-bin/xterm
 
+mount -o remount,rw /
+mkdir -p /usr/local/lib
+mkdir -p /usr/local/bin
+touch /usr/local/bin/xterm
+mount -o remount,ro /
+
 # Get proc going in the new namespace
 mount -t proc /proc
 
+mkdir -p $JAIL_DIR
 mount --bind $JAIL_DIR $JAIL_DIR
 
 mkdir -p $JAIL_DIR/bin
@@ -20,8 +24,12 @@ mkdir -p $JAIL_DIR/usr
 mkdir -p $JAIL_DIR/lib
 mkdir -p $JAIL_DIR/sbin
 mkdir -p $JAIL_DIR/sys
+mkdir -p $JAIL_DIR/etc
+mkdir -p $JAIL_DIR/home/root
 mkdir -p $JAIL_DIR/dev
 mkdir -p $JAIL_DIR/mnt
+mkdir -p $JAIL_DIR/proc
+mkdir -p $JAIL_DIR/tmp
 
 mount --bind /bin $JAIL_DIR/bin
 mount --bind /usr $JAIL_DIR/usr
@@ -29,6 +37,8 @@ mount --bind /lib $JAIL_DIR/lib
 mount --bind /sbin $JAIL_DIR/sbin
 mount --bind /sys $JAIL_DIR/sys
 mount --bind /dev $JAIL_DIR/dev
+mount --bind /tmp $JAIL_DIR/dev/mapper
+
 
 mount --bind $XLIB_DIR $JAIL_DIR/usr/local/lib
 mount --bind $XTERM_BIN $JAIL_DIR/usr/local/bin/xterm
